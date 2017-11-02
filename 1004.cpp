e1ae046c46ed07bfd0e8e97010d7fba265a4ae74
rezaulhsagar <rezaulhsagar@gmail.com>
2017-11-03 01:06:15 +0600

    ad-hoc

---
+++ 1004.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <bitset>
+#include <cassert>
+#include <cstdio>
+#include <queue>
+#include <set>
+#include <map>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <numeric>
+
+#define eps 1e-9
+#define pi acos(-1)
+#define pb push_back
+#define mp make_pair
+#define INF 10000000
+#define MOD 1000000007
+#define pause system("pause")
+#define mem(a,b) memset(a,b,sizeof(a))
+#define clock 1.0*clock()/CLOCKS_PER_SEC
+#define filein freopen("in.txt","r",stdin)
+#define fileout freopen("out.txt","w",stdout)
+#define fast std::ios::sync_with_stdio(false);cin.tie(NULL)
+
+using namespace std;
+
+typedef long long ll;
+
+int Set(int N, int p){return N|=(1<<p);}
+int Reset(int N, int p){return N&=~(1<<p);}
+bool Check(int N, int p){return N&(1<<p);}
+
+template<class T> inline void input(T &x) {
+    register char c = getchar();x = 0;
+    int neg = 0;
+    for(; ((c<48 || c>57) && c != '-'); c = getchar());
+    if(c=='-'){neg = 1;c = getchar();}
+    for(; c>47 && c<58 ; c = getchar()){x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x = -x;
+}
+
+const int dr[]={0,-1,0,1,-1,-1,1,1};
+const int dc[]={1,0,-1,0,1,-1,-1,1};
+const int M = 100000+5;
+
+int main(){
+	double sum = 0, c;
+	for(int i=0;i<12;i++){
+		cin>>c;
+		sum += c;
+	}
+	printf("$%.2f\n",sum / 12.0);
+    return 0;
+}
